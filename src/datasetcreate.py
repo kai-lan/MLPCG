bf67200ffb643e02f5a1b01bc208e4d5469058e6
osmanakar <osmanakar1123@hotmail.com>
2022-09-28 15:08:19 -0700

    misc

--- src/datasetcreate.py
+++ src/datasetcreate.py
@@ -20,17 +20,17 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-N", "--resolution", type=int, choices=[64, 128],
                     help="N or resolution of the training matrix", default = 128)
 parser.add_argument("-m", "--number_of_base_ritz_vectors", type=int,
-                    help="number of ritz vectors to be used as the base in the dataset", default=10000)
+                    help="number of ritz vectors to be used as the base for the dataset", default=10000)
 parser.add_argument("--sample_size", type=int,
-                    help="number of vectors to be created for dataset", default=20000)
+                    help="number of vectors to be created for dataset. I.e., size of the dataset", default=20000)
 parser.add_argument("--theta", type=int,
-                    help="see paper for.", default=500)
+                    help="see paper for the definition.", default=500)
 parser.add_argument("--small_matmul_size", type=int,
                     help="Number of vectors.", default=200)
 parser.add_argument("--dataset_dir", type=str,
                     help="path to the folder containing training matrix")
 parser.add_argument("--output_dir", type=str,
-                    help="folder that saves the dataset")
+                    help="path to the folder the training dataset to be saved")
 args = parser.parse_args()
 
 #%%
@@ -45,7 +45,10 @@ import pathlib
 pathlib.Path(args.output_dir).mkdir(parents=True, exist_ok=True) 
 
 #%% Load the matrix A
-A_file_name = args.dataset_dir + "/original_matA/A_origN"+str(N)+".bin"  #Change this one tho origional matrix (important)
+if N == 64:
+    A_file_name = args.dataset_dir + "/original_matA/A_origN"+str(N)+".bin"  
+elif N == 128:
+    A_file_name = args.dataset_dir + "/original_matA/A_oriN"+str(N)+".bin"  
 A = hf.readA_sparse(N, A_file_name,'f')
 CG = cg.ConjugateGradientSparse(A)
 rand_vec_x = np.random.normal(0,1, [N**3])
@@ -70,8 +73,8 @@ tri_diag[num_ritz_vectors-1,num_ritz_vectors-2]=sub_diagonal[num_ritz_vectors-2]
 
 #%% Calculating eigenvectors of the tridiagonal matrix
 print("Calculating eigenvectors of the tridiagonal matrix")
-eigvals, Q0 = np.linalg.eigh(tri_diag)
-eigvals = np.real(eigvals)
+ritz_vals, Q0 = np.linalg.eigh(tri_diag)
+ritz_vals = np.real(ritz_vals)
 Q0 = np.real(Q0)
 ritz_vectors = np.matmul(W.transpose(),Q0).transpose()
 
@@ -93,7 +96,7 @@ for_outside = int(args.sample_size/small_matmul_size)
 b_rhs_temp = np.zeros([small_matmul_size,N**3])
 cut_idx = int(num_ritz_vectors/2)+args.theta
 num_zero_ritz_vals = 0
-while eigvals[num_zero_ritz_vals] < 1.0e-8:
+while ritz_vals[num_zero_ritz_vals] < 1.0e-8:
     num_zero_ritz_vals = num_zero_ritz_vals + 1
     
 print("Creating Dataset ")
@@ -102,28 +105,15 @@ for it in range(0,for_outside):
     sample_size = small_matmul_size
     coef_matrix = np.random.normal(0,1, [num_ritz_vectors-num_zero_ritz_vals,sample_size])
     coef_matrix[0:cut_idx] = 9*np.random.normal(0,1, [cut_idx,sample_size])
-
     l_b = small_matmul_size*it
     r_b = small_matmul_size*(it+1)
-    print(it)
-    #b_rhs[l_b:r_b] = np.matmul(ritz_vectors[0:num_ritz_vectors-num_zero_ritz_vals].transpose(),coef_matrix[:,l_b:r_b]).transpose()
     #b_rhs[l_b:r_b] = np.matmul(ritz_vectors[0:num_ritz_vectors-num_zero_ritz_vals].transpose(),coef_matrix).transpose()
     b_rhs_temp = mat_mult(ritz_vectors[num_zero_ritz_vals:num_ritz_vectors].transpose(),coef_matrix).transpose()
 
-    #% % Making sure b is in the range of A
     for i in range(l_b,r_b):
-        if i%10 == 0:
-            print(i)
-        #b_rhs[0] = b_rhs[0] - sum(b_rhs[0][reduced_idx])/len(reduced_idx)
-        #b_rhs[0][zero_idxs]=0
         b_rhs_temp[i-l_b]=b_rhs_temp[i-l_b]/np.linalg.norm(b_rhs_temp[i-l_b])
-        #with open(project_data_folder3+'b_rhs_20000_20000_ritz_vectors_V2_for_3D_random_N'+str(dim-1)+'/'+str(i)+'.npy', 'wb') as f:
         with open(args.output_dir+'/b_'+str(i)+'.npy', 'wb') as f:
             np.save(f, np.array(b_rhs_temp[i-l_b],dtype=np.float32))
-        
-    print(norm(b_rhs_temp)**2)
-    time_cg_ml = int((time.time() - t0))
-    print("data creation at ",it, " took ", time_cg_ml, " seconds.")
 
 
 
