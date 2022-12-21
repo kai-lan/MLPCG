cd364cc75f5eb990379d19489da7aa9bb0baa99e
osmanakar <osmanakar1123@hotmail.com>
2022-12-21 21:45:53 +0300

    hf updated.

--- lib/helper_functions.py
+++ lib/helper_functions.py
@@ -16,51 +16,13 @@ import pressure_laplacian as pl
 
         
 
-
-def QTQ(Q):
-    dot_products = np.zeros([Q.shape[1],Q.shape[1]])
-    for i in range(Q.shape[0]):
-        for j in range(Q.shape[0]):
-            #This is just to look at the normality
-            dot_products[i][j] = np.dot(Q[:,i],Q[:,j])
-    return dot_products
-
-def QTQ_norm(Q):
-    s = 0.0
-    for i in range(Q.shape[0]):
-        for j in range(Q.shape[0]):
-            s = s + abs(np.dot(Q[:,i],Q[:,j]))
-    return s
-
-def orthonormality_check(Q):
-    s = 0.0
-    for i in range(Q.shape[0]):
-        for j in range(Q.shape[0]):
-            if i == j:
-                s = s + abs(np.dot(Q[i],Q[j])-1)  
-            else:
-                s = s + abs(np.dot(Q[i],Q[j]))  
-    return s
-
-def diagonality_check(A):
-    return np.linalg.norm(A-np.identity(A.shape[0]))
-    
-    
-def normalize_preconditioner(Q):
-    for i in range(Q.shape[0]):
-        Q[i] = Q[i]/np.linalg.norm(Q[i])
-        
-
-def AQ(A,mult_precond):
-    M = np.zeros(A.shape)
-    for i in range(A.shape[0]):
-        M[:,i] = mult_precond(A[i])
-    return M
-
-
-def off_diagonal_sum(A):
-    return sum(sum(A)) - sum(A.diagonal())
-
+def load_vector(data_folder_name):
+    if(os.path.exists(data_folder_name)):
+        r0 = np.fromfile(data_folder_name, dtype='double')
+        r1 = np.delete(r0, [0])
+        return r1        
+    else:
+        print("No file for exist named "+data_folder_name)
 
 def get_vec(file_rhs,normalize = False, d_type='double'):
     if(os.path.exists(file_rhs)):
@@ -83,26 +45,6 @@ def get_frame_from_source(n,data_folder_name,normalize = True, d_type='double'):
     else:
         print("No file for n = "+str(n)+" in data folder "+data_folder_name)
 
-    
-def get_frame(n,dim=65,pc="windows", type_=""):
-    if pc=="windows":
-        data_folder_name = "C:/Users/osman/OneDrive/research_teran/python/data/incompressible_flow_outputs3/output"+str(dim-1)
-    elif pc=="mac":
-        data_folder_name = "/Users/osmanakar/OneDrive/research_teran/python/data/incompressible_flow_outputs3/output"+str(dim-1)
-    
-    if type_ == "":    
-        data_folder_name = data_folder_name + "/"
-    else:
-        data_folder_name = data_folder_name + "_"+type_+"/"
-    
-    file_rhs = data_folder_name + "div_v_star_"+str(n)+".bin"
-    if(os.path.exists(file_rhs)):
-        r0 = np.fromfile(file_rhs, dtype='double')
-        r1 = np.delete(r0, [0])
-        norm_r = np.linalg.norm(r1)
-        return r1/norm_r
-    else:
-        print("No file for n = "+str(n))
 
 """
 template <typename T>
@@ -372,32 +314,7 @@ def ReadEigenSpMat(dim, filenameA):
         rows = rows + [ii]*(outerIdxPtr[ii+1] - outerIdxPtr[ii])
     return sparse.csr_matrix((data, (rows, cols)),[dim,dim])
 
-def make_ML_entry(b):
-    b_ML = b.copy()
-    b_ML = b_ML/np.linalg.norm(b)
-    return b_ML.reshape(1,65,65,1)
 
-def check_Q(Q, n=64):
-    pres_lap = pl.pressure_laplacian(n)
-    return np.matmul(Q,Q.transpose()), np.matmul(np.matmul(Q,pres_lap.A),Q.transpose())
-
-
-def extend_array(res_arr, mult_coeff):
-    res_arr0 = [res_arr[0]]
-    for i in range(1,len(res_arr)):
-        res_arr0 = res_arr0 + [res_arr[i]]*mult_coeff
-    return res_arr0
-
-
-
-def create_plot_bar_arr(eigvectors_coef, bar_range):
-    res_arr_bar_plot = []
-    dim2 = len(eigvectors_coef)
-    block_size = int(dim2/bar_range)
-    eigvectors_coef2 = eigvectors_coef**2
-    for i in range(bar_range):
-        res_arr_bar_plot = res_arr_bar_plot + [np.sqrt(sum(eigvectors_coef2[i*block_size:(i+1)*block_size]))]
-    return res_arr_bar_plot
 
 
 
@@ -411,210 +328,7 @@ def load_model_from_source(model_file_source):
     print("Loaded model from disk")
     return ml_model
 
-def load_model_from_machine(epoch_num, project_folder_general,project_folder_subname, dim=64 , threeD=False,  best_models=False):
-    dim2 = dim**2      
-    
-    if threeD:
-        legion_folder_name = "osman@legion.math.ucla.edu:~/projects/ML_preconditioner_project/MLCG_3D_N"+str(dim)+"/"
-        model_name = project_folder_subname+"/saved_models/MLCG_3D_N"+str(dim)+"_json_E"+str(epoch_num)+"/ " +project_folder_general+"saved_models/"
-    
-    if dim==64:
-        legion_folder_name = "osman@legion.math.ucla.edu:~/projects/ML_preconditioner_project/preconditioner_first_iter_MLV1/"
-        model_name = project_folder_subname+"/saved_models/preconditioner_first_iter_MLV1_json_E"+str(epoch_num)+"/ " +project_folder_general+"saved_models/"
-    elif dim==128:
-        legion_folder_name = "osman@legion.math.ucla.edu:~/projects/ML_preconditioner_project/preconditioner_first_iter_MLV1_N127/"
-        model_name = project_folder_subname+"/saved_models/preconditioner_first_iter_MLV1_N127_json_E"+str(epoch_num)+"/ " +project_folder_general+"/saved_models/" 
-    elif dim==256:
-        legion_folder_name = "osman@legion.math.ucla.edu:~/projects/ML_preconditioner_project/preconditioner_first_iter_MLV1_N255/"
-        model_name = project_folder_subname+"/saved_models/preconditioner_first_iter_MLV1_N255_json_E"+str(epoch_num)+"/ " +project_folder_general+"/saved_models/"
-    elif dim==512:
-        legion_folder_name = "osman@legion.math.ucla.edu:~/projects/ML_preconditioner_project/preconditioner_first_iter_MLV1_N511/"
-        model_name = project_folder_subname+"/saved_models/preconditioner_first_iter_MLV1_N511_json_E"+str(epoch_num)+"/ " +project_folder_general+"/saved_models/" 
-    else:
-        print("No such folder in legion machine")
-    
-    
 
-    os_return_val = os.system("scp -r "+ legion_folder_name+model_name)
-    if os_return_val != 0:
-        print("File not found in the legion machine")
-        return
-    
-    if threeD:
-        model_name = project_folder_general + "saved_models/MLCG_3D_N"+str(dim)+"_json_E"+str(epoch_num)+'/'
-
-    if dim==64:
-        if best_models:
-            model_name = project_folder_general + 'saved_models_best/preconditioner_first_iter_MLV1_json_E'+str(epoch_num)+'/'
-        else:
-            model_name = project_folder_general + 'saved_models/preconditioner_first_iter_MLV1_json_E'+str(epoch_num)+'/'
-    elif dim==128:
-        if best_models:
-            model_name = project_folder_general + 'saved_models_best/preconditioner_first_iter_MLV1_N127_json_E'+str(epoch_num)+'/'
-        else:
-            model_name = project_folder_general + 'saved_models/preconditioner_first_iter_MLV1_N127_json_E'+str(epoch_num)+'/'
-    elif dim==256:
-        if best_models:
-            model_name = project_folder_general + 'saved_models_best/preconditioner_first_iter_MLV1_N255_json_E'+str(epoch_num)+'/'
-        else:
-            model_name = project_folder_general + 'saved_models/preconditioner_first_iter_MLV1_N255_json_E'+str(epoch_num)+'/'
-    elif dim==512:
-        if best_models:
-            model_name = project_folder_general + 'saved_models_best/preconditioner_first_iter_MLV1_N511_json_E'+str(epoch_num)+'/'
-        else:
-            model_name = project_folder_general + 'saved_models/preconditioner_first_iter_MLV1_N511_json_E'+str(epoch_num)+'/'
-       
-
-    json_file = open(model_name + 'model.json', 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    model_PFI_V1 = keras.models.model_from_json(loaded_model_json)
-    # load weights into new model
-    model_PFI_V1.load_weights(model_name + "model.h5")
-    print("Loaded model from disk")
-    return model_PFI_V1
-
-def load_model_from_machine_V2(epoch_num, project_folder_general, project_name, project_folder_subname, machine="legion"):
-    if machine == "legion": 
-        machine_folder_name = "osman@legion.math.ucla.edu:~/projects/ML_preconditioner_project/"+project_name+"/"+ project_folder_subname+"/saved_models/"+project_name+ "_json_E" +str(epoch_num)+"/"
-    if machine == "hyde01":
-        machine_folder_name = "osman@legion.math.ucla.edu:~/projects/ML_preconditioner_project/"+project_name+"/"+ project_folder_subname+"/saved_models/"+project_name+ "_json_E" +str(epoch_num)+"/"
-
-        
-    model_folder_name = project_folder_general+"saved_models/"
-    os_return_val = os.system("scp -r "+ machine_folder_name+" "+model_folder_name)
-    if os_return_val != 0:
-        print("File not found in the legion machine")
-        return  
-    
-    model_name = model_folder_name+project_name+"_json_E"+str(epoch_num)+"/"
-    json_file = open(model_name + 'model.json', 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    model_PFI_V1 = keras.models.model_from_json(loaded_model_json)
-    # load weights into new model
-    model_PFI_V1.load_weights(model_name + "model.h5")
-    print("Loaded model from disk")
-    return model_PFI_V1
-
-def load_model_from_machine_V3(epoch_num, model_saving_place, project_name, project_folder_subname, machine="legion"):
-    if machine == "legion": 
-        machine_folder_name = "osman@legion.math.ucla.edu:~/projects/ML_preconditioner_project/"+project_name+"/"+project_folder_subname+"/saved_models/"+project_name+ "_json_E" +str(epoch_num)+"/"
-    if machine == "hyde01":
-        machine_folder_name = "oak@hyde01.dabh.io:~/projects/ML_preconditioner_project/"+project_name+"/"+project_folder_subname+"/saved_models/"+project_name+ "_json_E" +str(epoch_num)+"/"
-
-    
-    #model_folder_name = project_folder_general+"saved_models/"
-    model_name = model_saving_place+project_name + "_json_E"+str(epoch_num)+"/"
-    os_return_val = os.system("scp -r "+ machine_folder_name+" " + model_name)
-    if os_return_val != 0:
-        print("File not found in the legion machine")
-        return  
-    
-    #model_name = model_saving_place+project_name+"_json_E"+str(epoch_num)+"/"
-    json_file = open(model_name + 'model.json', 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    model_PFI_V1 = keras.models.model_from_json(loaded_model_json)
-    # load weights into new model
-    model_PFI_V1.load_weights(model_name + "model.h5")
-    print("Loaded model from disk")
-    return model_PFI_V1
-
-def first_residual_fast_old(b, Q,CG_):
-    #Q = Q.reshape([num_vectors,dim2])
-    A_tilde = np.matmul(Q,np.matmul(CG_.A, Q.transpose()))
-    return Q.transpose()*np.matmul(Q,b)/A_tilde
-    #return x_init 
-    
-def first_residual_fast(b, q, CG_):
-    #Q = Q.reshape([num_vectors,dim2])
-    A_tilde = np.matmul(Q,np.matmul(CG_.A, Q.transpose()))
-    return Q.transpose()*np.matmul(Q,b)/A_tilde
-    #return x_init 
-
-def ML_generated_iterative_subspace_solver(b,ml_model,CG_,max_it=100,verbose=True):
-    b1 = b.copy()
-    dim2 =len(b)
-    dim = int(np.sqrt(dim2))
-    x_sol = np.zeros(b.shape)
-    res_arr = []
-    Q = np.zeros([max_it,dim2])
-    for i in range(max_it):
-        b1_norm = np.linalg.norm(b1)
-        b1_normalized = b1/b1_norm
-        b_tf = tf.convert_to_tensor(b1_normalized.reshape([1,dim,dim,1]),dtype=tf.float32)
-        q = ml_model.predict(b_tf)[0,:,:,:].reshape([dim2]) #first_residual
-        Q[i]=q.copy()
-        #q = b1.copy()
-        #x = first_residual(b1,Q_)
-        x = first_residual_fast_old(b1,q,CG_)
-        x_sol = x_sol+x
-        b1 = b-CG_.multiply_A_sparse(x_sol)
-        res_arr = res_arr + [b1_norm]
-        #
-        if verbose:
-            print(i,b1_norm)
-    last_norm = np.linalg.norm(b1)
-    print("After max_it = ",max_it, " iterations ML-generated solver converged to ",last_norm)
-    return x_sol, res_arr
-
-def ML_generated_iterative_subspace_solver_A_conjugate(b,ml_model,CG_,max_it=100,verbose=True, num_previous_bases = 3):
-    b1 = b.copy()
-    dim2 =len(b)
-    dim = int(np.sqrt(dim2))
-    x_sol = np.zeros(b.shape)
-    res_arr = []
-    Q = np.zeros([max_it, dim2])
-    AQ = np.zeros([max_it, dim2])
-    Q_A_norms = np.zeros(max_it)
-    for i in range(max_it):
-        b1_norm = np.linalg.norm(b1)
-        b1_normalized = b1/b1_norm
-        b_tf = tf.convert_to_tensor(b1_normalized.reshape([1,dim,dim,1]),dtype=tf.float32)
-        q = ml_model.predict(b_tf)[0,:,:,:].reshape([dim2]) #first_residual
-        q_iter = q.copy()
-        for j in range(max(0,i-num_previous_bases),i):            
-            q_iter = q_iter - Q[j]*np.dot(q,AQ[j])/Q_A_norms[j]
-        Q[i] = q_iter.copy()
-        AQ[i] = np.matmul(CG_.A,Q[i])
-        Q_A_norms[i] = np.dot(Q[i], AQ[i])
-        #q = b1.copy()
-        #x = first_residual(b1,Q_)
-        x = first_residual_fast_old(b1,q_iter,CG_)
-        x_sol = x_sol+x
-        b1 = b-np.matmul(CG_.A,x_sol)
-        res_arr = res_arr + [b1_norm]
-        #
-        if verbose:
-            print(i,b1_norm)
-        
-    
-    last_norm = np.linalg.norm(b1)
-    print("After max_it = ",max_it, " iterations ML-generated solver converged to ",last_norm)
-    return x_sol, res_arr #, Q, AQ
-    
-
-def ML_generated_iterative_subspace_solver_test(ml_model, bs, CG_, max_it=100, tol=1e-6):
-    dim2 = len(bs[0])
-    #dim = int(np.sqrt(dim2))
-    bad_frames = []
-    res_arr_average = np.zeros(max_it)
-    for i in range(len(bs)):
-        b = bs[i].copy()
-        x_sol, res_arr = ML_generated_iterative_subspace_solver(b,ml_model,CG_,max_it,False)
-        res_arr_average = res_arr_average + np.array(res_arr)
-        if res_arr[-1]>tol:
-            print(i,res_arr[-1])
-            bad_frames = bad_frames+[i]
-    
-    res_arr_average = res_arr_average/len(bs)
-    
-    return res_arr_average, bad_frames
-    
-    
-    
-    
     
     
     
---
+++ src/create_ritz_vectors_from_lanczos_vectors.py
@@ -0,0 +1,123 @@
+import os
+import sys
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(1, dir_path+'/../lib/')
+
+import numpy as np
+import tensorflow as tf
+import scipy.sparse as sparse
+from numpy.linalg import norm
+import time
+import argparse
+
+import conjugate_gradient as cg
+import helper_functions as hf
+
+#os.environ["CUDA_VISIBLE_DEVICES"]= '' #not necessary
+
+#%% Get Arguments from parser
+parser = argparse.ArgumentParser()
+parser.add_argument("-N", "--resolution", type=int, choices=[64, 128],
+                    help="N or resolution of the training matrix", default = 128)
+parser.add_argument("-m", "--number_of_base_ritz_vectors", type=int,
+                    help="number of ritz vectors to be used as the base for the dataset", default=10000)
+parser.add_argument("--sample_size", type=int,
+                    help="number of vectors to be created for dataset. I.e., size of the dataset", default=20000)
+parser.add_argument("--theta", type=int,
+                    help="see paper for the definition.", default=500)
+parser.add_argument("--small_matmul_size", type=int,
+                    help="Number of vectors.", default=200)
+parser.add_argument("--lanczos_dir", type=str,
+                    help="path to the folder containing lanczoz vector")
+parser.add_argument("--output_dir", type=str,
+                    help="path to the folder the ritz vectors to be saved")
+parser.add_argument("--input_matrix_A_dir", type=str,
+                    help="path to the matrix_A file. It should be .bin file")
+args = parser.parse_args()
+
+#%%
+N = args.resolution
+
+num_ritz_vectors = args.number_of_base_ritz_vectors
+
+small_matmul_size = args.small_matmul_size
+
+#save output_dir
+import pathlib
+pathlib.Path(args.output_dir).mkdir(parents=True, exist_ok=True) 
+
+#%%
+A_file_name = args.input_matrix_A_dir
+A = hf.readA_sparse(N, A_file_name,'f')
+CG = cg.ConjugateGradientSparse(A)
+rand_vec_x = np.random.normal(0,1, [N**3])
+rand_vec = A.dot(rand_vec_x)
+
+
+#%%
+print("Loading Lanczos Vectors")
+W = np.zeros([num_ritz_vectors,N**3])
+
+for i in range(num_ritz_vectors):
+    if i%1000==0:
+        print(i)
+    lv_filename = args.lanczos_dir+"/"+str(i)+".bin"
+    W[i] = hf.load_vector(lv_filename)
+
+diagonal = np.zeros([num_ritz_vectors])
+sub_diagonal = np.zeros([num_ritz_vectors-1])
+for i in range(num_ritz_vectors-1):
+    Av = A.dot(W[i])
+    diagonal[i] = np.dot(W[i],Av)
+    sub_diagonal[i] = np.dot(Av,W[i+1])
+    
+Av = A.dot(W[num_ritz_vectors-1])
+diagonal[num_ritz_vectors-1] = np.dot(W[num_ritz_vectors-1],Av)
+
+
+#%% Create the tridiagonal matrix from diagonal and subdiagonal entries
+tri_diag = np.zeros([num_ritz_vectors,num_ritz_vectors])
+for i in range(1,num_ritz_vectors-1):
+    tri_diag[i,i] = diagonal[i]
+    tri_diag[i,i+1] = sub_diagonal[i]
+    tri_diag[i,i-1]= sub_diagonal[i-1]
+tri_diag[0,0]=diagonal[0]
+tri_diag[0,1]=sub_diagonal[0]
+tri_diag[num_ritz_vectors-1,num_ritz_vectors-1]=diagonal[num_ritz_vectors-1]
+tri_diag[num_ritz_vectors-1,num_ritz_vectors-2]=sub_diagonal[num_ritz_vectors-2]
+
+
+#%% Calculating eigenvectors of the tridiagonal matrix
+print("Calculating eigenvectors of the tridiagonal matrix")
+ritz_vals, Q0 = np.linalg.eigh(tri_diag)
+ritz_vals = np.real(ritz_vals)
+Q0 = np.real(Q0)
+print("Creating and saving ritz vectors...")
+ritz_vectors_temp = np.zeros([100,N**3])
+#%%
+num_for_loops = int(num_ritz_vectors/100)
+for i in range(100):
+    print("i = ",i)
+    ll = 100*i
+    rr = ll+100
+    print(ll)
+    #% %
+    #ritz_vectors[ll:rr] = np.matmul(W.transpose(),Q0[:,ll:rr]).transpose()
+    ritz_vectors_temp = np.matmul(W.transpose(),Q0[:,ll:rr]).transpose()
+
+    for i in range(ll,rr):
+        #with open(project_data_folder3+'ritz_vectors_10000_3D_N'+str(dim-1)+'/'+str(i)+'.npy', 'wb') as f:
+        with open(args.output_dir+'/'+str(i)+'.npy', 'wb') as f:
+            np.save(f, np.array(ritz_vectors_temp[i-ll],dtype=np.float32))  
+
+
+
+
+
+
+
+
+
+
+
+
